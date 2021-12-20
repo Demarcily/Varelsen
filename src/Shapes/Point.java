0d028b0b2f04035c86745e0da1057d967f11aa9f
Demarcily <60388907+Demarcily@users.noreply.github.com>
2021-12-20 09:34:49 +0100

    Color Variable and idea for type variable

--- src/Shapes/Point.java
+++ src/Shapes/Point.java
@@ -9,25 +9,36 @@ package Shapes;
 public class Point extends Shape{
   private int x;
   private int y;
+  private int color;
+  private int type;
 
-  public Point(int x, int y) {
+  public Point(int x, int y, int color, int type) {
     this.x = x;
     this.y = y;
+    this.color = color;
+    this.type = type;
   }
-
+  // Getters
   public int getX() {
     return x;
   }
-
-  public void setX(int x) {
-    this.x = x;
-  }
-
   public int getY() {
     return y;
   }
+  public int getColor() {
+    return color;
+  }
 
+  //Setters
+  public void setX(int x) {
+    this.x = x;
+  }
   public void setY(int y) {
     this.y = y;
   }
+  public void setColor(int color) {
+    this.color = color;
+  }
+
+
 }
--- src/SimulationModel/Cell.java
+++ src/SimulationModel/Cell.java
@@ -3,12 +3,12 @@ package SimulationModel;
 import Shapes.Point;
 import Shapes.Shape;
 
-
-
 public class Cell {
   private int x;
   private int y;
+  private int color = 0xFFFF00;
   private boolean alive;
+  private int type = (int) Math.round(Math.random()*10);
 
   public Cell(int x, int y, boolean alive) {
     this.x = x;
@@ -17,7 +17,7 @@ public class Cell {
   }
 
   public Shape getShape() {
-    return new Point(x, y);
+    return new Point(x, y, color, type);
   }
 
   public void update() {
@@ -25,7 +25,7 @@ public class Cell {
   }
 
   public String toString() {
-    return "Cell\nx:" + x + "\ny: " + y + "\n Alive: " + alive;
+    return "Cell\nx: " + x + "\ny: " + y + "\n Alive: " + alive;
   }
 
   public boolean getAlive() {
